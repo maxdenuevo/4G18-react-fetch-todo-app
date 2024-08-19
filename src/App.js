100feaf9f3b6a3ffa5c885178d94db3404cf95ff
maxdenuevo <maxihnen@gmail.com>
2024-08-19 16:38:19 -0400

    Co-authored-by: maxdenuevo <166054107+maxdenuevo@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+reportWebVitals(sendToVercelAnalytics);
+
+
+let todos = ["Make the bed", "Wash my hands", "Eat", "Walk the dog"];
+
+export function App() {
+	const [inputValue, setInputValue] = useState("");
+	const [addToArray, setAddToArray] = useState(todos);
+
+	const addTodo = e => {
+		if (e.key == "Enter") {
+			if (inputValue !== "") {
+				const todosNew = addToArray.concat(inputValue);
+				setAddToArray(todosNew);
+				setInputValue("");
+			} else alert("Insert a task");
+		}
+	};
+	const removeTodo = task => {
+		const removeItem = addToArray.filter(item => item !== task);
+		setAddToArray(removeItem);
+	};
+	const Todolist = () => {
+		if (addToArray.length > 0) {
+			return (
+				<div>
+					<ul className="list-group list-group-flush">
+						{addToArray.map(item => (
+							<li
+								className="list-group-item d-flex justify-content-between align-items-center"
+								key={item}>
+								{item}
+								<button
+									onClick={() => removeTodo(item)}
+									className="btn btn-link">
+									<i className="fas fa-times"></i>
+								</button>
+							</li>
+						))}
+					</ul>
+					<hr className="solid" />
+					<div className="float-left text-muted mt-2">
+						{addToArray.length} item left
+					</div>
+				</div>
+			);
+		} else {
+			return (
+				<div>
+					<p>No tasks, add a task</p>
+				</div>
+			);
+		}
+	};
+	return (
+		<div className="container text-center mt-5 justify-content-center">
+			<h1>todos</h1>
+			<div className="card cardtodos mx-auto">
+				<div className="card-body">
+					<div className="input-group mb-3 input-group-lg">
+						<input
+							type="text"
+							className="form-control"
+							placeholder="What do you need?"
+							value={inputValue}
+							onChange={e => setInputValue(e.target.value)}
+							onKeyUp={addTodo}
+						/>
+					</div>
+					<Todolist />
+				</div>
+			</div>
+		</div>
+	);
 }
+
 
 export default App;
